37848c8aa7f24c463426deec9cb987c6f994a78b
felipebevi <felipe@9ideias.com>
2023-06-05 14:57:06 -0300

    tc

---
+++ EPSC.ps1
@@ -0,0 +1,12 @@
+
+$files=Get-ChildItem "C:\l\"
+Foreach($file in $files)
+{
+Write-Host "Attaching File :- " $file
+$attachment = New-Object System.Net.Mail.Attachment –ArgumentList C:\l\$file
+$ReportEmail.Attachments.Add($attachment)
+$attachment.Dispose()
+}
+$SMTPInfo.Send($ReportEmail)
+Write-host 'Mail Sent Successfully'
+
